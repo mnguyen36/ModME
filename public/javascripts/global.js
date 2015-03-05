e815718a8bad936dddf866ca15dd2b5633dccee8
mnguyen <mnguyen@sharecare.com>
2015-03-05 01:20:46 -0500

    adding key bind events to up down

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -3,14 +3,34 @@ var userListData = [];
 
 // DOM Ready =============================================================
 $(document).ready(function() {
+
     var nav = $('.nav li');
+    var currentIndex = 0;
 
     nav.each(function(index) {
+        console.log(window.location.hash);
+
+        var currentTab = $(this).find('a');
+        if (currentTab.attr('data-hash')==window.location.hash.replace('#', '')){
+            $(this).toggleClass('selected');
+            updateContent();
+            currentIndex = index;
+        }
+        if (window.location.hash == ''){
+            window.location.hash= '/';
+            $(nav.get(0)).toggleClass('selected');
+            currentIndex=0;
+        }
+        //$(this.lastChild.children).attr('data-hash')
         $(this).on("click", function (e) {
-            console.log($(nav.get(index)).children().children().attr('data-hash'));
-            window.location.hash=$(nav.get(index)).children().children().attr('data-hash');
+            window.location.hash=$(e.target).attr('data-hash');
+            nav.each(function(index){
+                $(nav.get(index)).removeClass('selected');
+            });
             $(nav.get(index)).toggleClass('selected');
+            currentIndex = index;
         });
+
     });
 
     $(window).on('hashchange', function() {
@@ -25,7 +45,31 @@ $(document).ready(function() {
                 break;
         }
     });
+
+    $(document).on('keydown', function(e) {
+        console.log(e.keyCode);
+        console.log(currentIndex);
+        nav.each(function(index){
+            $(nav.get(index)).removeClass('selected');
+        });
+        $(nav.get(currentIndex-1)).toggleClass('selected');
+        console.log($(nav.get(currentIndex-1)).find('a'));
+        updateContent();
+    });
 });
+
+function updateContent() {
+    hideContent();
+    switch(window.location.hash){
+        case '#userlist':
+            $('.center-column').slideDown();
+            updateTable();
+            break;
+        case '#/':
+            $('.homescreen').slideDown();
+            break;
+    }
+}
 
 function hideContent(){
     $.each($('.page-container').children(), function(){
@@ -45,30 +89,5 @@ function updateTable(){
             tableContent += '</tr>';
         });
         $('#user-table tbody').html(tableContent);
-    });
-}
-
-// Functions =============================================================
-
-// Fill table with data
-function populateTable() {
-
-    // Empty content string
-    var tableContent = '';
-
-    // jQuery AJAX call for JSON
-    $.getJSON( '/users/userlist', function( data ) {
-
-        // For each item in our JSON, add a table row and cells to the content string
-        $.each(data, function(){
-            tableContent += '<tr>';
-            tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.username + '">' + this.username + '</a></td>';
-            tableContent += '<td>' + this.email + '</td>';
-            tableContent += '<td><a href="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
-            tableContent += '</tr>';
-        });
-
-        // Inject the whole content string into our existing HTML table
-        $('#userList table tbody').html(tableContent);
     });
 }
